16d459d3c284d02317ad18a44059b07062a06a47
Tropik42 <aviavova94@gmail.com>
2021-05-22 19:33:54 +0300

    fixed file upload

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -16,7 +16,6 @@ class FileController {
             } else {
                 const queryParentFile = await pool.query("SELECT file_id FROM files WHERE name = $1 AND user_id = $2 AND path = $3", [req.user.id, req.user.id, ''])
                 parentFile = queryParentFile.rows[0].file_id
-                console.log(parentFile);
             }
             let file
             if (!parent) {
@@ -37,7 +36,7 @@ class FileController {
                 await fileService.createDir(fileInfo)
             }
             file = file.rows[0]
-            console.log('file = ', file);
+            // console.log('file = ', file);
             return res.json(file)
         } catch (e) {
             console.log(e)
@@ -52,14 +51,13 @@ class FileController {
             if (!parent) {
                 let fileId
                 fileId = await pool.query("SELECT file_id FROM files WHERE name = $1 AND user_id = $2 AND path = $3", [req.user.id, req.user.id, ''])
-                console.log(fileId.rows[0].file_id);
-                // files = await pool.query("SELECT FROM files WHERE file_id = (SELECT file_id FROM files WHERE name = $1 AND user_id = $2 AND path = $3)", [req.user.id.toString(), req.user.id, ''])
+                console.log('если нет parent, fileId = ', fileId.rows[0].file_id);
                 files = await pool.query("SELECT * FROM files WHERE parent = $1", [fileId.rows[0].file_id])
             } else {
                 files = await pool.query("SELECT * FROM files WHERE parent = $1", [parent])
             }
             files = files.rows
-            console.log(files);
+            // console.log('fetchFiles возвращает', files);
             return res.json(files) 
         } catch (e) {
             console.log(e)
@@ -69,32 +67,47 @@ class FileController {
 
     async uploadFile(req, res) {
         try {
+            const {parent} = req.body
+            console.log();
             const file = req.files.file
-            // console.log(file);
+            console.log('Файл прислан такой: ', file);
+            let parentFile
+            if (parent) {
+                const queryParentFile = await pool.query("SELECT * FROM files WHERE file_id = $1", [parent])
+                parentFile = queryParentFile.rows[0]
+                console.log('Если есть родитель, он такой: ', parentFile)
+            } else {
+                const queryParentFileId = await pool.query("SELECT file_id FROM files WHERE name = $1 AND user_id = $2 AND path = $3", [req.user.id, req.user.id, ''])
+                // console.log('найденный file_id: ', queryParentFileId);
+                const queryParentFile = await pool.query("SELECT * FROM files WHERE file_id = $1", [queryParentFileId.rows[0].file_id])
+                parentFile = queryParentFile.rows[0]
 
-            const parent = await pool.query('SELECT FROM files')
-            const user = await pool.query('SELECT * FROM users WHERE user_id = $1', [req.user.id])
-            if (user.usedSpace + file.size > user.diskSpace) {
+                console.log('Если родителя нет, присваивается это: ', parentFile);
+            }
+            const queryUser = await pool.query('SELECT * FROM users WHERE user_id = $1', [req.user.id])
+            const user = queryUser.rows[0]
+            console.log('Юзера БД прислала такого: ', user);
+            if (user.usedspace + file.size > user.diskspace) {
                 return res.status(400).json({message: 'There is no space on the disk'})
             }
             
-            user.usedSpace = user.usedSpace + file.size
+            // user.usedSpace = user.usedspace + file.size
             let filePath;
             if (parent) {
-                filePath = path.join(path.resolve('files'), user.id.toString(), parent.path.toString(), file.name)
+                filePath = path.join(path.resolve('files'), user.user_id.toString(), parentFile.path.toString(), file.name)
             } else {       
-                filePath = path.join(path.resolve('files'), user.id.toString(), file.name)
+                filePath = path.join(path.resolve('files'), user.user_id.toString(), file.name)
             }
 
-            if (fs.exystsSync(filePath)) {
+            if (fs.existsSync(filePath)) {
                 return res.status(400).json({message: 'File already exists'})
             }
             file.mv(filePath)
 
             const type = file.name.split('.').pop()
             const dbFile = await pool.query(
-                "INSERT INTO files (name, type, size, path, parent, user_id) VALUES ($1, $2, $3, $4, $5, $6)", 
-                [file.name, type, file.size, parent.path, parent.id, req.user.id])
+                "INSERT INTO files (name, type, size, path, parent, user_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *", 
+                [file.name, type, file.size, parentFile.path, parentFile.file_id, req.user.id])
             
             res.json(dbFile)
 
