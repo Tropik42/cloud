7b20718b31e42b4882b3f37da3bfa9d2ed702cf6
Tropik42 <aviavova94@gmail.com>
2021-08-01 17:09:16 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -7,8 +7,8 @@ const cors = require('cors')
 const app = express()
 const PORT = config.get("serverPort")
 
-app.use(fileUpload({}))
 app.use(cors())
+app.use(fileUpload({}))
 app.use(express.json())
 app.use(express.static('static'))
 
